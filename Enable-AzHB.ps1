857af32b949ac481e4429e7c9b9cddd9478a01d6
pzohaycuoi <pzohaycuoi@gmail.com>
2022-01-10 16:45:30 +0700

    base on distro, then push AHB accordingly

--- Enable-AzHB.ps1
+++ Enable-AzHB.ps1
@@ -1,7 +1,7 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
-    [string]$FilePath
+  [Parameter()]
+  [string]$FilePath
 )
 
 # create csv result file
@@ -16,10 +16,10 @@ $data = Import-Csv -Path $FilePath
 # loop through each row
 foreach ($row in $data) {
   # put row's data into object for export to result file
-    $rowData = [PSCustomObject]@{
-    VmName = $row.VmName
+  $rowData = [PSCustomObject]@{
+    VmName        = $row.VmName
     ResourceGroup = $row.ResourceGroup
-    Subscription = $row.Subscription
+    Subscription  = $row.Subscription
   }
 
   # get current sub of the CLI
@@ -48,30 +48,70 @@ foreach ($row in $data) {
 
   # check if vm is windows or not
   if ($vmData.StorageProfile.osDisk.OsType -ne "Windows") {
-    $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue "VM $($row.VmName) OS is $($vmData.StorageProfile.osDisk.OsType)"
-    $rowData
-    continue
-  }
-
-  # if LicenseType Attribute is not defined then set else skip to next row
-  if (($vmData.LicenseType -eq '') -or ($vmData.LicenseType -eq $null)) {
-    $vmData.LicenseType = "Windows_Server"
-    try {
-      Update-AzVM -ResourceGroupName $row.ResourceGroup -VM $vmData -ErrorAction Stop
-      $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $true
-      $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
-      $rowData
+    # if it's Linux then invoke AZ Vm command to find the distro name
+    # AHB only applied to RHEL and SUSE
+    $invokeAzVm = Invoke-AzVMRunCommand -ResourceGroupName $row.ResourceGroup -VMName $row.VmName -CommandId "RunShellScript" -ScriptPath "./getVmDistro.sh"
+    if ($invokeAzVm.Value.Message -clike "*rhel*") {
+      $vmData.LicenseType = "RHEL_BYOS"
+      $rowData | Add-Member -NotePropertyName "OS" -NotePropertyValue "rhel"
+      try {
+        Update-AzVM -ResourceGroupName $row.ResourceGroup -VM $vmData -ErrorAction Stop
+        $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $true
+        $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
+        $rowData
+      }
+      catch {
+        $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $false
+        $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
+        $rowData
+      }
     }
-    catch {
-      $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $false
+    elseif ($invokeAzVm.Value.Message -clike "*suse*") {
+      $vmData.LicenseType = "SLES_BYOS"
+      $rowData | Add-Member -NotePropertyName "OS" -NotePropertyValue "suse"
+      try {
+        Update-AzVM -ResourceGroupName $row.ResourceGroup -VM $vmData -ErrorAction Stop
+        $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $true
+        $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
+        $rowData
+      }
+      catch {
+        $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $false
+        $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
+        $rowData
+      }
+    }
+    else {
+      # AHB only support for Redhat and SUSE
+      $rowData | Add-Member -NotePropertyName "OS" -NotePropertyValue "other distro"
+      $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue "The VM OS is supported for AHB"
       $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
       $rowData
+      continue
     }
   }
   else {
-    $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $true
-    $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
-    $rowData
-    continue
+    $rowData | Add-Member -NotePropertyName "OS" -NotePropertyValue "Windows"
+    # if LicenseType Attribute is not defined then set else skip to next row
+    if (($vmData.LicenseType -eq '') -or ($vmData.LicenseType -eq $null)) {
+      $vmData.LicenseType = "Windows_Server"
+      try {
+        Update-AzVM -ResourceGroupName $row.ResourceGroup -VM $vmData -ErrorAction Stop
+        $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $true
+        $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
+        $rowData
+      }
+      catch {
+        $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $false
+        $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
+        $rowData
+      }
+    }
+    else {
+      $rowData | Add-Member -NotePropertyName "Result" -NotePropertyValue $true
+      $rowData | Export-Csv -Path $resultFile.FullName -NoTypeInformation -Append -Force
+      $rowData
+      continue
+    }
   }
 }
